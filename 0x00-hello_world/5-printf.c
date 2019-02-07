e2396d85bca123ec59ae046adb03d26d5b7c7ab6
Grayson Stiegler <682@holbertonschool.com>
2019-02-07 00:11:59 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main -main 
  *
- *Return: Always 0
+ *Return:  0
  *
  */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
