181cbba40feb2fc30590899dfbee13d6dd7d0549
Grayson Stiegler <682@holbertonschool.com>
2019-02-11 23:27:51 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_alphabet- this is printing the alphabet in lower case
+ *
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+
+	int c;
+
+	for(c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
