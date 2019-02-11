6c670b2ba1fe43a6ca57ce47ac2bb7f4fe1030e9
Grayson Stiegler <682@holbertonschool.com>
2019-02-11 18:29:13 +0000

    0-holberton.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - main
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_puthcar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	return (0);
+}
+x
