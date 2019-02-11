0608350a5c340226c112d6ca2b9890ccda9cbbbf
Grayson Stiegler <682@holbertonschool.com>
2019-02-11 23:37:18 +0000

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,14 @@
 /**
  * print_alphabet- this is printing the alphabet in lower case
  *
- * Return: 0
+ * Return: 0.
  */
 void print_alphabet(void)
 {
 
 	int c;
 
-	for(c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
 	_putchar('\n');
 }
