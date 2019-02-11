2044e69700e434e6ccf104ea8d033b9dac0ade61
Grayson Stiegler <682@holbertonschool.com>
2019-02-11 22:39:24 +0000

    holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,6 +1,3 @@
-#ifndef HOLBERTON
-#define HOLBERTON
-
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,5 +9,3 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
-#endif
