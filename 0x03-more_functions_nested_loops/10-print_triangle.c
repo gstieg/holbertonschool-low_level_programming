48328d553ae56322797784520de9bda8361d45b5
Grayson Stiegler <graysonstieger@gmail.com>
2019-02-15 00:17:20 +0000

    print a triangle

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_triangles - prints a triangle
+ *
+ * @size: size
+ */
+void print_triangle(int size)
+{
+	int a,b;
+
+	if (size > 0)
+	{
+		for (b = 0; b < size; b++)
+		{
+			for (a = 0; a < size; a++)
+			{
+				if (a < size - b - 1)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
