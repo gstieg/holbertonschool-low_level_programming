ac2bce7cc59760881e91d1b62e45302df8e659a4
Grayson Stiegler <graysonstieger@gmail.com>
2019-02-19 20:56:27 +0000

    prints half of a string

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints half string
+ *
+ * @str: value of input
+ */
+void puts_half(char *str)
+{
+	int count = 0;
+	int half = 0;
+
+	while (str[count] != '\0')
+	{
+		count += 1;
+	}
+	if (count % 2 == 0)
+		half = count /2;
+	else
+		half = (count + 1) / 2;
+	count = count - 1;
+	while (half <= count)
+	{
+		_putchar(str[half]);
+		half++;
+	}
+	_putchar('\n');
+}
