99c9f493f7075e4392ccba43792db765f6a3c914
Grayson Stiegler <graysonstieger@gmail.com>
2019-02-19 20:48:35 +0000

    advanced

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _atoi - convert string to int
+ *
+ * @s: value of string
+ *
+ * Return: return the int
+ */
+int _atoi(char *s)
+{
+	int count = 0;
+	unsigned int tmp = 0;
+	int tmp_n = 1;
+
+	while (s[count] != '\0')
+	{
+		if (s[count] == '-')
+			tmp_n = tmp_n * -1;
+		if (s[count] >= '0' && s[count] <= '9')
+			tmp = tmp * 10 + (s[count] - '0');
+		if (s[count] == ';')
+			break;
+		count++;
+	}
+	return (tmp = tmp * tmp_n);
+}
