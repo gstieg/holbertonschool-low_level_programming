cd95f1620f428ed179301904b6b211c8dd61e4bd
Grayson Stiegler <graysonstieger@gmail.com>
2019-02-22 00:06:36 +0000

    changes all lowercase letters to uppercase

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - prints out upper case
+ *
+ * @str: input
+ *
+ * Return: string
+ */
+char *string_toupper(char *)
+{
+	int count = 0; n = 0;
+
+	while (str[n] != '\0')
+		n++;
+	while (count < n )
+	{
+		if (str[count] >= 'a' && str[count] <= 'z')
+			str[count] = str[count] - 32;
+		count++;
+	}
+	return (str);
+}
