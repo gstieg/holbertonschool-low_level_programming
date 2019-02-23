93007257cb88b8b4d922392207be89ba84b63c88
Grayson Stiegler <graysonstieger@gmail.com>
2019-02-23 04:09:38 +0000

    changed output

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -1,24 +1,24 @@
 #include "holberton.h"
 
 /**
- * rot13 - convert to rot 13
+ * rot13 - convert to rot13
  *
  * @str: input
  *
- * Return: String convert
+ * Return: string convert
  */
 char *rot13(char *str)
 {
 	int count = 0, count_a;
-	char *one = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyz";
+	char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	while (str[count] != '\0')
 	{
 		count_a = 0;
-		while (one[count_a] != '\0')
+		while (alpha[count_a] != '\0')
 		{
-			if (str[count] == one[count_a])
+			if (str[count] == alpha[count_a])
 			{
 				str[count] = rot13[count_a];
 				break;
