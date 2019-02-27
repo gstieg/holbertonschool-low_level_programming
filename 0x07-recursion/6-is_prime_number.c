c7280a0ea791f7145f9bfad132e8517a5c8d3731
Grayson Stiegler <682@holbertonschool.com>
2019-02-27 22:54:54 +0000

    inception

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * recursion - make the recursion work
+ *
+ * @n; input
+ *
+ * @div: divide
+ *
+ * Return: prime number
+ */
+int recursion(int n, int div)
+{
+	if (div < 0)
+		return (-1);
+	if (div == 0)
+		return (-1);
+	else if (n % div != 0)
+	{
+		div--;
+		return (recursion(n, div));
+	}
+	return (div);
+}
+
+/**
+ * is_prime_number - find out if its prime
+ *
+ * @n: input
+ *
+ * Return: square root
+ */
+int is_prime_number(int n)
+{
+	int div = n -1;
+
+	if (recursion(n, div) == 1)
+		return (1);
+	return (0);
+}
