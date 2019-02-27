200135f02f1cc95541080d642011d776f503e963
Grayson Stiegler <682@holbertonschool.com>
2019-02-27 21:20:17 +0000

    returns length of string

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion -  returns length of string
+ *
+ * @s: input
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (_strlen_recursion(s + 1) + 1);
+	}
+	return (0);
+}
