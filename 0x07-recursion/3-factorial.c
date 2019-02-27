9115c8351e302fd849d3ccf32f4ebb605240dbc4
Grayson Stiegler <682@holbertonschool.com>
2019-02-27 21:24:24 +0000

    returns the factorial of any given number

---
+++ 0x07-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * factorial - returns the facotrial of a given number
+ *
+ * @n: input
+ *
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+	if (n > 1)
+		n = n * factorial(n - 1);
+	else if (n < 0)
+		return (-1);
+	return(n);
+}
