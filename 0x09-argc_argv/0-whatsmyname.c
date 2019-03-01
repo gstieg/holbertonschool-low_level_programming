1c2497a3506c77f5eec6a6120dc8024180e6fe72
Grayson Stiegler <682@holbertonschool.com>
2019-03-01 21:33:44 +0000

    program that prints its name

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints programs name
+ *
+ * @argc: input 1
+ *
+ * @argv: input 2
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >= 0)
+		printf("%s/n", argv[0]);
+	return (0);
+}
