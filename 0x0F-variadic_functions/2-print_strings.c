fe5b96357aba9b479ca0476466e83512b731fb5e
Grayson Stiegler <682@holbertonschool.com>
2019-03-15 21:15:15 +0000

    fixed spacing and punc

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -27,7 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("(nil)");
 		}
-		if (separator != NULL && a < n -1)
+		if (separator != NULL && a < n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");
