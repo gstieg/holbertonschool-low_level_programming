5a0a9d8e7e684a2de3839463fd0850e18c669e41
Grayson Stiegler <682@holbertonschool.com>
2019-03-21 19:32:31 +0000

    prints all elements of list_t

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - print the items in the list
+ *
+ * @h: input
+ *
+ * Return: nodes
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *c_list = h;
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+	while (c_list != NULL)
+	{
+		if (c_list->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", c_list->len, c_list->str);
+		c_list = c_list->next;
+		count++;
+	}
+	return (count);
+}
