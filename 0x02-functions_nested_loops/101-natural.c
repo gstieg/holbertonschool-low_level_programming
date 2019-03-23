e5ac9c616195e1dfdb3f19d357a268f3d21fcdaf
Grayson Stiegler <682@holbertonschool.com>
2019-03-23 02:46:07 +0000

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - increase and decrease
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int c, d;
+
+	for (c = 0; c < 1024; c++)
+	{
+		if (c % 3 == 0 || c % 5 == 0)
+			d = d + a;
+	}
+	printf("%d\n", d);
+	return (0);
+}
