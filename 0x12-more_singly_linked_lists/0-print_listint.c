71f8b3188f71b3b866f879d07c2625b9c78e4417
Grayson Stiegler <682@holbertonschool.com>
2019-03-25 17:13:42 +0000

    Write a function that prints all the element

---
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "more_lists.h"
+
+/**
+ * print_listint- print numbers of the list
+ *
+ * @h: input
+ *
+ * Return: nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *list = h;
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+	while (list != NULL)
+	{
+		printf("%d\n", list->n);
+		list = list->next;
+		count++;
+	}
+	return (count);
+}
