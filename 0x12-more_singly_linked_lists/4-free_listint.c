e706dcec460930ac2dfc7def360787ab85cb5394
Grayson Stiegler <682@holbertonschool.com>
2019-03-25 21:42:25 +0000

    free the list

---
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "more_lists.h"
+
+/**
+ * free_listint - free list
+ *
+ * @head: input
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *hold;
+
+	while (head != NULL)
+	{
+		hold = head->next;
+		free(head);
+		head = hold;
+	}
+}
