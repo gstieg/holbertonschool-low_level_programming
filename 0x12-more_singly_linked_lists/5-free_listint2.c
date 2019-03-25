d4ea90084aaa80ef0fe5305aef338828f0f54e77
Grayson Stiegler <682@holbertonschool.com>
2019-03-25 21:47:16 +0000

    free the whole list

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "more_lists.h"
+
+/**
+ * free_listint2 - free all
+ *
+ * @head: input
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *hold;
+
+	if (*head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		hold = (*head)->next;
+		free(*head);
+		*head = hold;
+	}
+}
