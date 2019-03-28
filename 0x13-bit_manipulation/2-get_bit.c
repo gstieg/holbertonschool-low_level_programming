a052b81796534b1357e1db31a54ff85a6bc5b306
Grayson Stiegler <682@holbertonschool.com>
2019-03-28 22:03:36 +0000

    get bit of a given index

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * get_bit - get the bit
+ *
+ * @n: input
+ *
+ * @index: index of binary
+ *
+ * Return: the binary number
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if ((sizeof(unsigned long int) * 8) < index)
+	     return (-1);
+	while (index > 0)
+	{
+		n = n >> 1;
+		index--;
+	}
+	if (n & 1)
+		return (1);
+	return (0);
+}
