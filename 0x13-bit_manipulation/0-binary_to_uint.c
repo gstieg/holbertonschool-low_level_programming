df3ac0f293227a5ce586a20fb7abcf1255e981ca
Grayson Stiegler <682@holbertonschool.com>
2019-03-28 21:43:46 +0000

    write a function that converts binary to a unsigned int

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * binary_to_unit - convert to binary
+ *
+ * @b:  input for binary
+ *
+ * Return: the converted number of binary
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int count = 0;
+	unsigned int num = 0, num2 = 1;
+
+	if (b == NULL)
+		return (0);
+	while (b[count] != '\0')
+		count++;
+	count--;
+	while (count >= 0)
+	{
+		if (b[count] != '0' && b[count] != '1')
+			return (0);
+		num += (b[count] - '0') * num2;
+		count--;
+		num2 *= 2;
+	}
+	return (num);
+}
