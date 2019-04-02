f6271f2a31e04334322f92216664eb11ed552ea4
Grayson Stiegler <682@holbertonschool.com>
2019-04-02 23:47:23 +0000

    Create a function that creates a file.

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * create_file -  create a file
+ *
+ * @filename: input for filename
+ *
+ * @text_content: input of context
+ *
+ * Return: 1 if success or -1 for failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int ab, num = 1, count = 0;
+
+	if (filename == NULL)
+		return (-1);
+	ab = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (ab == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close(ab);
+		return (1);
+	}
+	else
+	{	while (text_content[count] != '\0')
+		{
+			count++;
+		}
+		num = write(ab, text_content, count);
+		if (num == -1)
+			return (-1);
+	}
+	close(ab);
+	return (1);
+}
