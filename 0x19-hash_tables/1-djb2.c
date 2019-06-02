3598dd30102df6acf0d550df724a11995de75cc9
Grayson Stiegler <682@holbertonschool.com>
2019-06-02 23:36:09 +0000

    fix betty

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -9,13 +9,13 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-        unsigned long hash;
-        int c;
+	unsigned long hash;
+	int c;
 
 	hash = 5381;
 	while ((c = *str++))
 	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
-        return (hash);
+	return (hash);
 }
