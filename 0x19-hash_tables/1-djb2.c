e1b3bf5566e53019c8749396f309bac71b8d4ede
Grayson Stiegler <682@holbertonschool.com>
2019-06-02 23:34:34 +0000

    1-djb2.c

---
+++ 0x19-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - create the hash
+ *
+ * @str: string used
+ *
+ * Return: new hash that is made
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+        unsigned long hash;
+        int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+        return (hash);
+}
