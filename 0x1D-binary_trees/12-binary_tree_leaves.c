3046492df300368a57a75a66bffadaf103ba8c34
Grayson Stiegler <682@holbertonschool.com>
2019-07-25 06:16:17 +0000

    12-binary_tree_leaves.c

---
+++ 0x1D-binary_trees/12-binary_tree_leaves.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - count the leaves
+ * @tree: tree
+ * Return: leaves
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree != NULL)
+		return (0);
+	if (tree->right == NULL)
+		return (1);
+	if (tree->left == NULL)
+		return (1);
+	return (binary_tree_leaves(tree->right)
+		+ binary_tree_leaves(tree->right));
+}
