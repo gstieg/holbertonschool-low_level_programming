26007835e22f6e7005381ea80f7db70b3e8ab03a
Grayson Stiegler <682@holbertonschool.com>
2019-07-26 07:15:09 +0000

    fix syntax

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -8,8 +8,6 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left, right;
-
 	if (tree == NULL)
 		return (0);
 	if (binary_tree_height(tree->left) > binary_tree_height(tree->right))
